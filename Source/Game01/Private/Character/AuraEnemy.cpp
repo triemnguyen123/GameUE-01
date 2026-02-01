db7b7ce33950b3ba20bedfdde9f28e224841217e
triemnguyen <triemnguyen213@gmail.com>
2026-02-01 22:58:44 +0700

    update highlight outline view on enemy

--- Source/Game01/Private/Character/AuraEnemy.cpp
+++ Source/Game01/Private/Character/AuraEnemy.cpp
@@ -2,11 +2,20 @@
 
 
 #include "Character/AuraEnemy.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "Game01/Game01.h"
 
 AAuraEnemy::AAuraEnemy() 
 {
     GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+    
+    AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+    AbilitySystemComponent->SetIsReplicated(true);
+    AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+    
+    AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
 
 void AAuraEnemy::HighLightActor() 
--- Source/Game01/Public/Character/AuraCharacterBase.h
+++ Source/Game01/Public/Character/AuraCharacterBase.h
@@ -4,19 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "AuraCharacterBase.generated.h"
 
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 UCLASS(Abstract)
-class GAME01_API AAuraCharacterBase : public ACharacter
+class GAME01_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()	
 
 public:
 	AAuraCharacterBase();
+        virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+        UAttributeSet* GetAttributeSet() const {return AttributeSet;};
 	
 protected:
 	virtual void BeginPlay() override;
 	
 	UPROPERTY(EditAnywhere, Category="Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
+    
+    UPROPERTY()
+    TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+    
+    UPROPERTY()
+    TObjectPtr<UAttributeSet> AttributeSet;
+    
 };
