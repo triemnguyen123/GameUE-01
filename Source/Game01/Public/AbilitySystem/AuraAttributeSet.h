16cbd22037658d2bacb4f3e46d76e32fc7f64a93
triemnguyen <triemnguyen213@gmail.com>
2026-02-02 00:31:48 +0700

    update Attribute Set linked server to client with health,Mana property through GAS REPNOTIFY, DORELIFETIME

---
+++ Source/Game01/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME01_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+public:
+    UAuraAttributeSet();
+    //~ khai bao cac bien co the gui from client -> server
+    virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty> &OutLifetimeProps) const override;
+    
+    UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
+    FGameplayAttributeData Health;
+    
+    UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth, Category="Vital Attributes")
+    FGameplayAttributeData MaxHealth;
+    
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category="Vital Attributes")
+    FGameplayAttributeData Mana;
+
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category="Vital Attributes")
+    FGameplayAttributeData MaxMana;
+    
+    UFUNCTION()
+    void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+    UFUNCTION()
+    void OnRep_MaxHealth(const FGameplayAttributeData& MaxOldHealth) const;
+    
+    UFUNCTION()
+    void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+    UFUNCTION()
+    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+    
+};
