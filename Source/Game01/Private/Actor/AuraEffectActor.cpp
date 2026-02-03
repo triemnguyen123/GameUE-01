bd62cf5ae1b280f6a09fdd3dd69333499bbf7811
triemnguyen <triemnguyen213@gmail.com>
2026-02-04 00:29:29 +0700

    update success testing with potionHeath collision with character

--- Source/Game01/Private/Actor/AuraEffectActor.cpp
+++ Source/Game01/Private/Actor/AuraEffectActor.cpp
@@ -3,25 +3,48 @@
 
 #include "Actor/AuraEffectActor.h"
 
-// Sets default values
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+
 AAuraEffectActor::AAuraEffectActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+ 
+    PrimaryActorTick.bCanEverTick = false;
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+    SetRootComponent(Mesh);
+    Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+    Sphere->SetupAttachment(GetRootComponent());
+    
 }
 
-// Called when the game starts or when spawned
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent *OverlappedComponent, 
+    AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) 
+{
+    //todo: Change this to apply gameplayEffect , for now u can using const_cast as a hack!
+    if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor)) 
+    {
+        const UAuraAttributeSet* AuraAttributes = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+       
+        UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributes);
+        MutableAuraAttributeSet->SetHealth(AuraAttributes->GetHealth() + 25.f);
+        Destroy();
+    }
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent *OverlappedComponent, 
+    AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex) 
+{
+    
+}
+
 void AAuraEffectActor::BeginPlay()
 {
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AAuraEffectActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+    Super::BeginPlay();
+    Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+    Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
 }
 
